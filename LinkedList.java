8498b7a258a49efde5da49f7140e63ad7109bb8b
jatin711-debug <jatinmahajan711@gmail.com>
2021-03-14 02:49:02 +0530

    edit-`

--- LinkedList.java
+++ LinkedList.java
@@ -50,7 +50,63 @@ public class LinkedList{
         current.next = newNode;
         }
         }
-       
     }
 
+    public boolean contains(int value){
+        Node current = head;
+        while(current != null){
+            if(current.value == value) return true;
+            current = current.next;
+        }
+        return false; 
+    }
+
+    public int getByIndex(int index){
+        if(index < 0) throw new IndexOutOfBoundsException();
+        else{
+            Node current = head;
+        for(int i = 0; i<index;i++){
+            if(current == null || current.next == null){
+                throw new IndexOutOfBoundsException();
+            }
+            current = current.next;
+        }
+        return current.value;
+    }
+        }
+
+    public void removeFromFront(){
+        if(head != null) head = head.next;
+        if(head == null) tail = null;
+    }
+
+    public void removeFromBack(){
+        if(head == null) return;
+        else if(head.equals(tail)){
+            head = null;
+            tail = null;
+        }else{
+            Node current = head;
+        while(current.next != tail){
+            current = current.next;
+        }
+        tail = current;
+        current.next = null;
+        }  
+    }
+
+    public void removeAtIndex(int index){
+        if(index<0) throw new IndexOutOfBoundsException();
+        else if(index == 0) this.removeFromFront();
+        else{
+            Node current = head;
+        for(int i=0;i<index-1;i++){
+            if(current == null) throw new IndexOutOfBoundsException();
+            current = current.next;
+        }
+        current.next = current.next.next;
+        if(current.next == null) tail = current;
+        }
+        
+    }
 }
