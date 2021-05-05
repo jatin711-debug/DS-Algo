3f94e6cb9df2451ed1700a61c3197a2ef3866fe5
jatin711-debug <69040077+jatin711-debug@users.noreply.github.com>
2021-05-04 22:21:07 -0400

    modifying in Dll

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -266,30 +266,6 @@ public class DoublyLinkedList {
      * @param args Command line arguments
      */
     public static void main(String args[]) {
-      DoublyLinkedList myList = new DoublyLinkedList();
-      myList.insertHead(13);
-      myList.insertHead(7);
-      myList.insertHead(10);
-      myList.display(); // <-- 10(head) <--> 7 <--> 13(tail) -->
-  
-      myList.insertTail(11);
-      myList.display(); // <-- 10(head) <--> 7 <--> 13 <--> 11(tail) -->
-  
-      myList.deleteTail();
-      myList.display(); // <-- 10(head) <--> 7 <--> 13(tail) -->
-  
-      myList.delete(7);
-      myList.display(); // <-- 10(head) <--> 13(tail) -->
-  
-      myList.insertOrdered(23);
-      myList.insertOrdered(67);
-      myList.insertOrdered(3);
-      myList.display(); // <-- 3(head) <--> 10 <--> 13 <--> 23 <--> 67(tail) -->
-      myList.insertElementByIndex(5, 1);
-      myList.display(); // <-- 3(head) <--> 5 <--> 10 <--> 13 <--> 23 <--> 67(tail) -->
-      myList.clearList();
-      myList.display();
-      myList.insertHead(20);
-      myList.display();
+      
     }
   }
